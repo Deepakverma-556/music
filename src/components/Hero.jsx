fe537a05f8811e574e09dc438ee7d421ed7422fc
Deepak Verma <deepakradialcode@gmail.com>
2025-01-20 21:20:25 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import Header from '../common/Header'
+import CustomButton from '../common/CustomButton'
+import { ALPHABET_LIST } from '../utils/Helper'
+import hero from '../assets/images/webp/hero.webp'
+import profile from '../assets/images/webp/profile.webp'
+import { DownArrow } from '../utils/icons'
+import { useNavigate } from 'react-router-dom'
+
+const Hero = () => {
+    const [domain, setDomain] = useState(null);
+    const [text, setText] = useState("HIT ME HARD AND SOFT")
+    const [alphaText,setAlphaText] = useState()
+    const navigate = useNavigate()
+    const handleDomainChange = (newDomain) => {
+        setDomain(newDomain);
+        navigate(`?value=${newDomain}`);
+        setText(`HIT ME HARD AND ${newDomain.toUpperCase()}`)
+    };
+    const handleChange = (newText) => (
+        setAlphaText(`${newText}`)
+    )
+    return (
+        <div className='pt-[10px] pb-10'>
+            <Header />
+            <div className='max-w-[1160px] mx-auto px-4'>
+                <div className='flex items-center gap-[15px] pt-[17px] max-xl:overflow-x-auto pb-2'>
+                    <div className='flex items-center gap-[5px]'>
+                        <CustomButton customOnClick={()=>handleDomainChange("all")} myClass="!text-xs px-[13.48px] py-[5.84px] !bg-black text-white hover:text-black hover:!bg-transparent" text="All" />
+                        <CustomButton customOnClick={()=>handleDomainChange("pop")} myClass="!text-xs text-customBlack py-[5.84px] px-[11.37px] hover:!bg-customBlack hover:text-white" text="Pop" />
+                        <CustomButton customOnClick={()=>handleDomainChange("rock")} myClass="!text-xs text-customBlack py-[5.84px] px-[11.8px] hover:!bg-customBlack hover:text-white" text="Rock" />
+                        <CustomButton customOnClick={()=>handleDomainChange("music")} myClass="!text-xs text-customBlack py-[5.84px] px-[16.7px] hover:!bg-customBlack hover:text-white group flex items-center gap-[5px]" text="More" icon={<DownArrow myClass="group-hover:stroke-white transition-all duration-300" />} />
+                    </div>
+                    <div className='flex items-center gap-[2px]'>
+                        {ALPHABET_LIST.map(function (item, index) {
+                            return <a onClick={()=> handleChange(item)} key={index} href="#link" className='flex items-center hover:bg-customBlack size-[29px] justify-center rounded-full transition-all duration-300 hover:text-white hover:font-medium text-black text-xs leading-[18px]'>{item}</a>
+                        })}
+                    </div>
+                </div>
+                <div className='bg-customBlack rounded-[22px] flex pl-12 pr-[43px] justify-between pt-[38px] mt-[35px] relative pb-[43px] max-sm:flex-wrap max-sm:pt-4 max-sm:px-5 max-sm:pb-20'>
+                    <h1 className='font-montserrat text-5xl leading-[58.51px] text-white font-bold max-lg:text-4xl max-sm:text-center max-sm:text-3xl'>{text}</h1>
+                    <img src={hero} alt="hero" className='size-[261px] max-lg:size-48 max-sm:mx-auto max-sm:mt-4' />
+                    <div className='absolute flex items-center gap-[26px] -bottom-16 max-lg:-bottom-10 max-sm:-bottom-6'>
+                        <img src={profile} alt="profile" className='size-[206px] max-lg:size-32 max-sm:size-20' />
+                        <div>
+                            <p className='font-semibold text-[32px] max-lg:text-2xl leading-[42px] text-white max-sm:text-lg'>Billie Eilish {alphaText}</p>
+                            <p className='font-montserrat font-medium text-base leading-5 text-white pt-[5px] max-lg:pt-0 pb-[21px] max-sm:text-sm'>Relesed May 17, 2024</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        lg: "1025px",
+        md: "770px",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"]
+      },
+      colors: {
+        gray: "#DBDBF3",
+        lightGray: "#31374D1A",
+        customBlack:"#14191C"
+      }
+    },
+  },
+  plugins: [],
+}
