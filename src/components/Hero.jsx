b4fdb40404393ad00dc1ae246f5153b43913f026
Deepak Verma <deepakradialcode@gmail.com>
2025-01-22 14:49:03 +0530

    add params

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../common/Header'
 import CustomButton from '../common/CustomButton'
 import { ALPHABET_LIST } from '../utils/helper'
@@ -10,6 +10,27 @@ import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
 const Hero = () => {
     const [activeParams, setActiveParams] = useSearchParams('value')
     const value = activeParams.get('value')?.toUpperCase()
+    const [selectId, setSelectId] = useState()
+    const navigate = useNavigate()
+    const { id = "all" } = useParams()
+
+    const handleChange = (id) => {
+        setSelectId(id);
+        navigate(`/${id}`);
+    };
+    
+    useEffect(() => {
+        setSelectId(id)
+    }, [id])
+
+    const getHeading = () =>
+    ({
+        pop: "Hit Me Hard and Pop",
+        rock: "Hit Me Hard and Rock",
+        all: "Hit Me Hard and Soft",
+        more: "Hit Me Hard and More",
+    }[selectId] || "Hit Me Hard and Soft");
+
 
     const handleDomainChange = (value) => {
         setActiveParams({ value: value.toLowerCase() })
@@ -21,10 +42,10 @@ const Hero = () => {
             <div className='max-w-[1160px] mx-auto px-4'>
                 <div className='flex items-center gap-[15px] pt-[17px] max-xl:overflow-x-auto pb-2'>
                     <div className='flex items-center gap-[5px]'>
-                        <CustomButton myClass={`!text-xs px-[13.48px] py-[5.84px] hover:!bg-customBlack hover:text-white`} text="All" />
-                        <CustomButton myClass={`!text-xs text-customBlack py-[5.84px] px-[11.37px] hover:!bg-customBlack hover:text-white`} text="Pop" />
-                        <CustomButton myClass={`!text-xs text-customBlack py-[5.84px] px-[11.8px] hover:!bg-customBlack hover:text-white`} text="Rock" />
-                        <CustomButton myClass={`!text-xs text-customBlack py-[5.84px] px-[9.2px] hover:!bg-customBlack hover:text-white group flex items-center gap-[5px]`} text="More" icon={<DownArrow myClass={`group-hover:stroke-white transition-all duration-300`} />} />
+                        <CustomButton customOnClick={() => handleChange("all")} myClass={`${selectId === "all" ? "bg-black text-white" : ""} !text-xs px-[13.48px] py-[5.84px] hover:!bg-customBlack hover:text-white`} text="All" />
+                        <CustomButton customOnClick={() => handleChange("pop")} myClass={`${selectId === "pop" ? "bg-black text-white" : ""} !text-xs text-customBlack py-[5.84px] px-[11.37px] hover:!bg-customBlack hover:text-white`} text="Pop" />
+                        <CustomButton customOnClick={() => handleChange("rock")} myClass={`${selectId === "rock" ? "bg-black text-white" : ""} !text-xs text-customBlack py-[5.84px] px-[11.8px] hover:!bg-customBlack hover:text-white`} text="Rock" />
+                        <CustomButton customOnClick={() => handleChange("more")} myClass={`${selectId === "more" ? "bg-black text-white" : ""} !text-xs text-customBlack py-[5.84px] px-[9.2px] hover:!bg-customBlack hover:text-white group flex items-center gap-[5px]`} text="More" icon={<DownArrow myClass={`group-hover:stroke-white transition-all duration-300`} />} />
                     </div>
                     <div className='flex items-center gap-[2px]'>
                         {ALPHABET_LIST.map(function (item, index) {
@@ -33,7 +54,7 @@ const Hero = () => {
                     </div>
                 </div>
                 <div className='bg-customBlack rounded-[22px] flex pl-12 pr-[43px] justify-between pt-[38px] mt-[35px] relative pb-[43px] max-sm:flex-wrap max-sm:pt-4 max-sm:px-5 max-sm:pb-20'>
-                    <h1 className='font-montserrat text-5xl leading-[58.51px] uppercase text-white font-bold max-lg:text-4xl max-sm:text-center max-sm:text-3xl'>hit me hard and</h1>
+                    <h1 className='font-montserrat text-5xl leading-[58.51px] uppercase text-white font-bold max-lg:text-4xl max-sm:text-center max-sm:text-3xl'>{getHeading()}</h1>
                     <img src={hero} alt="hero" className='size-[261px] max-lg:size-48 max-sm:mx-auto max-sm:mt-4 pointer-events-none' />
                     <div className='absolute flex items-center gap-[26px] -bottom-16 max-lg:-bottom-10 max-sm:-bottom-6'>
                         <img src={profile} alt="profile" className='size-[206px] max-lg:size-32 max-sm:size-20 pointer-events-none' />
